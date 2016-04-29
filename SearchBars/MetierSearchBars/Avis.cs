eae1155ed3481f0b259616c415dc158accd84cc4
ivschmidt <ivan.schmidt@etu.udamail.fr>
2016-04-29 15:21:18 +0200

    Classes du métier(propriétés + constructeur) + Classe date enlevée + classe Bar a finir

---
+++ SearchBars/MetierSearchBars/Avis.cs
@@ -0,0 +1,20 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetierSearchBars
+{
+    public class Avis
+    {
+        public int Note { get; set; }
+        public string Description { get; set; }
+
+        public Avis(int note, string desc)
+        {
+            Note = note;
+            Description = desc;
+        }
+    }
+}
---
+++ SearchBars/MetierSearchBars/Boisson.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetierSearchBars
+{
+    public class Boisson
+    {
+        List<Ingredient> composition;
+        public Type Type{ get; set; }
+        public string Nom { get; set; }
+        public float Prix { get; set; }
+        public string Marque { get; set; }
+        public int DegreAlcool { get; set; }
+
+        public Boisson(string nom, float prix, string marque, int degreA, Type type)
+        {
+            composition = new List<Ingredient>();
+            Nom = nom;
+            Prix = prix;
+            Marque = marque;
+            DegreAlcool = degreA;
+            Type = type;
+        }
+    }
+}
