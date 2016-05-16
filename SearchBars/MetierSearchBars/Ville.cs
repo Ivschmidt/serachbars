6ba487222714739df1023a7f4e92bd3a6e3efc4b
picoiffard <pierre.coiffard@etu-udamail.fr>
2016-05-16 17:21:25 +0200

    ajout de la classe Ville (oublié dans le dernier commit)

---
+++ SearchBars/MetierSearchBars/Ville.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetierSearchBars
+{
+    public class Ville
+    {
+        public string Nom { get; private set; }
+        public CoordonneesGPS GPS { get; private set; }
+
+        private List<Bar> listBar = new List<Bar>();
+
+        public Ville(string nom, CoordonneesGPS gps)
+        {
+            Nom = nom;
+            GPS = gps;
+        }
+
+        public void ajouterBar(Bar bar)
+        {
+            listBar.Add(bar);
+        }
+    }
+}
