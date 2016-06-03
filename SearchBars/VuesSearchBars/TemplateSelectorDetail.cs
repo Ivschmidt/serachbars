7ced9f29776f8b48dad104751a314d61b1ea7cb9
picoiffard <pierre.coiffard@etu-udamail.fr>
2016-06-03 15:18:36 +0200

    add de TSdetail (oubli desole)

---
+++ SearchBars/VuesSearchBars/TemplateSelectorDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace VuesSearchBars
+{
+    public class TemplateSelectorDetail : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if(item == null)
+            {
+                return DTVille;
+            }
+            return DTDetail;
+        }
+
+        public DataTemplate DTVille { get; set; }
+
+        public DataTemplate DTDetail { get; set; }
+    }
+}
