e634a830f7196e5a4671d8340ada2453229e8b25
picoiffard <pierre.coiffard@etu-udamail.fr>
2016-06-17 10:36:42 +0200

    add du converter

---
+++ SearchBars/VuesSearchBars/Converters/GPS2LocationConverter.cs
@@ -0,0 +1,26 @@
+﻿using MetierSearchBars;
+using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace VuesSearchBars.Converters
+{
+    class GPS2LocationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            CoordonneesGPS gps = (CoordonneesGPS)value;
+            return new Location(gps.Latitude, gps.Longitude);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
